b96292bfa6212acf1f96fd751caec56e2590ec0e
NicolaMazzaferroCoimaf <nicola.mazzaferro@coimaf.com>
2025-02-12 12:45:43 +0100

    aggiunti log

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,38 @@ Route::get('/logs/error', [LogController::class, 'getErrorLog']);
 
 //! TEST
 Route::get('/test', [TestController::class, 'test']);
+
+// LOG
+Route::get('/logs', function () {
+    $logPath = storage_path('logs');
+    $logFiles = File::files($logPath);
+
+    // Filtra i file che contengono il prefisso per prodotti o brand
+    $filteredLogs = collect($logFiles)->filter(function ($file) {
+        return str_contains($file->getFilename(), 'prestashop_products_job_log_') || 
+               str_contains($file->getFilename(), 'prestashop_details_job_log_') || 
+               str_contains($file->getFilename(), 'prestashop_brand_job_log_') ||
+               str_contains($file->getFilename(), 'prestashop_stock_job_log_') ||
+               str_contains($file->getFilename(), 'prestashop_images_job_log_') ||
+               str_contains($file->getFilename(), 'prestashop_customers_job_log_') ||
+               str_contains($file->getFilename(), 'prestashop_categories_job_log_');
+    })->map(function ($file) {
+        return [
+            'name' => $file->getFilename(),
+            'size' => $file->getSize(),
+            'last_modified' => date('Y-m-d H:i:s', $file->getMTime()),
+        ];
+    });
+
+    return response()->json($filteredLogs);
+});
+
+Route::get('/logs/{filename}', function ($filename) {
+    $filePath = storage_path("logs/$filename");
+
+    if (!File::exists($filePath)) {
+        return response()->json(['error' => 'File not found'], 404);
+    }
+
+    return Response::download($filePath, $filename);
+});
